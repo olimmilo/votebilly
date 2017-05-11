58793f17d7ac1176195dc186d261386b157a4cd5
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-05-11 19:54:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 		
 	</head>
 	<body>
-		<img src="IMG_1997.JPG" alt="KOMRADE BILLY KOMANDS YOU TO RISE">
-		<img src="IMG_1998.JPG" alt="KOMRADE BILLY SERVES ALL">
+		<img src="images/IMG_1997.JPG" alt="KOMRADE BILLY KOMANDS YOU TO RISE">
+		<img src="images/IMG_1998.JPG" alt="KOMRADE BILLY SERVES ALL">
 		<form action="work.php" method="post">
 			Will you vote for Billy?: <input type="button" value="Yes">  <input type="button" value="No"><br>
 			Say something about Billy: <input type="text" name="COMMENT"><br>
