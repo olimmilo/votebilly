731798096f27bb95581ea6ca3bcbd84e5d14d0e3
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-05-11 19:41:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
 <html>
-
-<body>
-
-<form action="work.php" method="post">
-Vertical Angle (E): <input type="text" name="E"><br>
-Azimuth angle (A): <input type="text" name="A"><br>
-Height (Z): <input type="text" name="H"><br>
-X coordinate of the anntenna (X<sub>L</sub>): <input type="text" name="X"><br>
-Y coordinate of the anntenna (Y<sub>L</sub>): <input type="text" name="Y"><br>
-<input type="submit">
-</form>
-
-
-
-</body>
+	<head>
+		
+	</head>
+	<body>
+		<form action="work.php" method="post">
+			Vertical Angle (E): <input type="text" name="E"><br>
+			Azimuth angle (A): <input type="text" name="A"><br>
+			Height (Z): <input type="text" name="H"><br>
+			X coordinate of the anntenna (X<sub>L</sub>): <input type="text" name="X"><br>
+			Y coordinate of the anntenna (Y<sub>L</sub>): <input type="text" name="Y"><br>
+			<input type="submit">
+		</form>
+	</body>
 </html>
