60ffd740c746070678abbb890cf8d4a4586ef307
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-05-11 19:49:28 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
 		
 	</head>
 	<body>
+		<img src="IMG_1997.JPG" alt="KOMRADE BILLY KOMANDS YOU TO RISE">
+		<img src="IMG_1998.JPG" alt="KOMRADE BILLY SERVES ALL">
 		<form action="work.php" method="post">
-			Vertical Angle (E): <input type="text" name="E"><br>
-			Azimuth angle (A): <input type="text" name="A"><br>
-			Height (Z): <input type="text" name="H"><br>
-			X coordinate of the anntenna (X<sub>L</sub>): <input type="text" name="X"><br>
-			Y coordinate of the anntenna (Y<sub>L</sub>): <input type="text" name="Y"><br>
+			Will you vote for Billy?: <input type="button" value="Yes">  <input type="button" value="No"><br>
+			Say something about Billy: <input type="text" name="COMMENT"><br>
 			<input type="submit">
 		</form>
 	</body>
