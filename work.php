d39d08f6f6f9ec20b95517d1f9be345cdd3a2e87
Maisy Wilcox <milowilcox@hanovernorwichschools.org>
2017-05-11 21:22:33 -0400

    Update work.php

--- work.php
+++ work.php
@@ -16,6 +16,13 @@
 	<body> 
 		<?php
 			$COMMENT=$_POST["COMMENT"];
+$file = 'comments.php';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+$current .= "Maisy\n";
+// Write the contents back to the file
+file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
 		?>
 		<script language="javascript">
    			 window.location.href = "http://votebilly.cf"
